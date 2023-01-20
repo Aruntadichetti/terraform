07d88a6d1482b5ed5ef92f7dbee68a403ae21588
arun.tadichetti <arun.tadichetti@gmail.com>
2023-01-20 16:52:14 +0530

    my second commit

--- terrafrom.tf
+++ terrafrom.tf
@@ -3,7 +3,3 @@ provider "aws" {
     access_key = "AKIA2XI63C64BJ3IE3H4"
     secret_key = "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"
 }
-
-resource "aws_vpc" "myfirstvpc" {
-  cidr_block = "10.0.0.0/16"
-}
